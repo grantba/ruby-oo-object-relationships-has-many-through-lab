864361a29656f4cd712effabafe3cf23df084804
Blaire <grant.blaire@gmail.com>
2020-12-06 22:16:08 -0600

    commit tree 4e351f1eb271f081ee7ae1a496c6c3d1dbdc6176

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,29 @@
+class Appointment
 
+    attr_accessor :date, :patient, :doctor
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    # def doctor
+    #     Doctor.all.select do |dr|
+    #         appointment.dr == self
+    #     end
+    # end
+
+    # def patient
+    #     Patient.all.select do |pt|
+    #         appointment.pt == self
+    #     end
+    # end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
 
+    attr_accessor :name, :appointment, :doctor
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor) 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.patient == self
+        end
+    end
+
+    def doctors
+        appointments.collect {|appt| appt.doctor}
+    end
+
+end
